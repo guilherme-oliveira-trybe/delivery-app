002a4d1fa1069d60f4d3bc242ed3492be2416c51
WalterTNFC <walter.tad@hotmail.com>
2022-10-27 16:57:40 -0300

    feat: Sales Migration

---
+++ back-end/src/database/migrations/3.salesMigration.js
@@ -0,0 +1,57 @@
+module.exports = {
+  up: async (QueryInterface, Sequelize) => {
+    await QueryInterface.createTable('sales', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      seller_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        foreignKey: true,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      total_price: {
+        type: Sequelize.DECIMAL(9,2),
+        allowNull: false,
+      },
+      delivery_address: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      delivery_number: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+      sale_date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.STRING(50),
+        allowNull: false,
+      },
+    });
+  },
+  
+  down: async (QueryInterface, _Sequelize) => {
+    await QueryInterface.dropTable('sales');
+  },
+}; 
