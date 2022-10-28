4e4001c52c8b293aced0652df50cd5e8a3b91835
Cleiton <cleitonrmacedo@gmail.com>
2022-10-28 19:11:52 -0300

    refactor: Colocado os data-testsid

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
 
-function Navbar() {
+function NavBar() {
   return (
     <nav className="nav-bar">
       <div className="box">
-        <a href="/customer/products/">PRODUTOS</a>
-        <a href="/customer/orders/">MEUS PEDIDOS</a>
+        <a
+          href="/customer/products/"
+          data-testid="customer_products__element-navbar-link-products"
+        >
+          PRODUTOS
+        </a>
+        <a
+          href="/customer/orders/"
+          data-testid="customer_products__element-navbar-link-orders"
+        >
+          MEUS PEDIDOS
+        </a>
       </div>
       <div className="box">
-        <a href="/8">Nome Usuário</a>
-        <a href="/">Sair</a>
+        <a
+          href="/8"
+          data-testid="customer_products__element-navbar-user-full-name"
+        >
+          Nome Usuário
+        </a>
+        <a
+          href="/"
+          data-testid="customer_products__element-navbar-link-logout"
+        >
+          Sair
+        </a>
       </div>
     </nav>
   );
 }
 
-export default Navbar;
+export default NavBar;
