43abd6259c2ef5e83fdcc32ddc685f088c95422d
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-28 14:38:20 -0300

    feat: model User

---
+++ back-end/src/database/models/user.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING(255),
+    },
+    email: {
+      type: DataTypes.STRING(255),
+    },
+    password: {
+      type: DataTypes.STRING(255),
+    },
+    role: {
+      type: DataTypes.STRING(255),
+    }
+  }, {
+    timestamps: false,
+    tableName: 'users'
+  });
+
+
+  User.associate = (models) => {
+    User.hasMany(models.Sales, {
+      foreignKey: 'id',
+    });
+  };
+
+
+  return User;
+};
