f9109b4683aab70f98734c335a1e7f4c476c14b4
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-28 12:39:27 -0300

    feat: criação do componente de tabela genérico

---
+++ front-end/src/components/Table.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+
+export default function Table({ orders, needButton }) {
+  return (
+    <section>
+      <table>
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-total</th>
+            {needButton && <th>Remover Item</th>}
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order, index) => (
+            <tr key={ index }>
+              <td>{ index }</td>
+              <td>{ order.name }</td>
+              <td>{ order.amount }</td>
+              <td>{ order.price }</td>
+              <td>{ order.amount * order.price }</td>
+              {needButton && <td><button type="button">Remover</button></td>}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <h3>
+        {orders.reduce((acc, curr) => {
+          acc += (curr.amount * curr.price);
+          return acc;
+        }, 0)}
+      </h3>
+    </section>
+  );
+}
+
+Table.propTypes = {
+  orders: PropTypes.arrayOf(PropTypes.instanceOf(Object)).isRequired,
+  needButton: PropTypes.bool.isRequired,
+};
