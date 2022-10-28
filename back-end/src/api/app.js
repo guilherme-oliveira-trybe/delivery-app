cda20a5c9986a15f0d0431459205eb496752e474
Guilherme Fernandes <97487898+guilherme-ac-fernandes@users.noreply.github.com>
2022-10-28 15:10:25 -0300

    Update app.js

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+app.use(express.json());
+
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
-module.exports = app; 
+module.exports = app;
