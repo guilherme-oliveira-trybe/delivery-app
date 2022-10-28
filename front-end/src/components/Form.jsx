c36737e74ffc45c08825796e978abaa05fa2e78c
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-28 14:12:43 -0300

    feat(20): criação inicial do redirecionamento

--- front-end/src/components/Form.jsx
+++ front-end/src/components/Form.jsx
@@ -1,13 +1,17 @@
 // import PropTypes from 'prop-types';
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Form() {
   const [seller, setSeller] = useState('');
   const [deliveryAddress, setDeliveryAddress] = useState('');
   const [deliveryNumber, setDeliveryNumber] = useState('');
+  const history = useHistory();
 
   const handleClick = () => {
     console.log('envia o pedido, fazer axios.post');
+    const { id: orderId } = 'post fake que retorna o id contendo o TOKEN';
+    history.push(`/customer/orders/${orderId}`);
   };
 
   return (
