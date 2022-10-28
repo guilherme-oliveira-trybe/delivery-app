c624366447fa1c8b221867ef5943a6ee4f18023a
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-28 13:24:06 -0300

    feat(17): criação do formulário de venda

---
+++ front-end/src/components/Form.jsx
@@ -0,0 +1,59 @@
+// import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+export default function Form() {
+  const [seller, setSeller] = useState('');
+  const [deliveryAddress, setDeliveryAddress] = useState('');
+  const [deliveryNumber, setDeliveryNumber] = useState('');
+
+  const handleClick = () => {
+    console.log('envia o pedido, fazer axios.post');
+  };
+
+  return (
+    <form>
+      <label htmlFor="seller">
+        P. Vendedora Responsável:
+        <select
+          name="seller"
+          id="seller"
+          value={ seller }
+          onChange={ ({ target: { value } }) => setSeller(value) }
+        >
+          <option value="V1">Vendedor 1</option>
+          <option value="V2">Vendedor 2</option>
+        </select>
+      </label>
+
+      <label htmlFor="delivery_address">
+        Endereço:
+        <input
+          type="text"
+          data-testid=""
+          value={ deliveryAddress }
+          id="delivery_address"
+          onChange={ ({ target: { value } }) => setDeliveryAddress(value) }
+        />
+      </label>
+
+      <label htmlFor="delivery_number">
+        Número:
+        <input
+          type="text"
+          data-testid=""
+          value={ deliveryNumber }
+          id="delivery_number"
+          onChange={ ({ target: { value } }) => setDeliveryNumber(value) }
+        />
+      </label>
+      <button
+        type="button"
+        onClick={ handleClick }
+      >
+        FINALIZAR PEDIDO
+      </button>
+    </form>
+  );
+}
+
+Form.propTypes = {};
