18cd8a2bdbad58e1564bc898862c7b960d842252
WalterTNFC <walter.tad@hotmail.com>
2022-10-31 14:50:03 -0300

    feat: Login Navigation

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,7 +1,18 @@
+import { useHistory } from 'react-router-dom';
+
 export default function Login() {
+  const history = useHistory();
+  console.log('ola');
   return (
     <div>
       <h1>Login</h1>
+      <button
+        data-testid="common_login__button-register"
+        type="button"
+        onClick={ () => history.push('/register') }
+      >
+        Ainda não tenho conta
+      </button>
     </div>
   );
 }
