f07a81d081c9bf03d5c48e43136c461841892f1d
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-10-31 14:56:02 -0300

    feat: página de order pelo usuário logado

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,7 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import OrderCard from '../components/customerOrders/OrderCard';
+import Navbar from '../components/NavBar';
+
 export default function Orders() {
+  const [customerOrder, setCustomerOrder] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCustomerOrders = async () => {
+      const url = 'http://www.localhost:3001/customer/orders/1';
+      const response = await fetch(url);
+      const data = await response.json();
+      setCustomerOrder(data);
+    };
+    fetchCustomerOrders();
+    setLoading(false);
+  }, []);
+
+  console.log(customerOrder);
+
   return (
     <div>
-      <h1>Orders</h1>
+      <Navbar />
+      { loading && <span>Loading</span>}
+      { !loading
+      && customerOrder.map(({ id, userId, status, saleDate, totalPrice }, index) => (
+        <OrderCard
+          key={ id }
+          saleId={ id }
+          userId={ userId }
+          order={ `${index + 1}` }
+          status={ status }
+          saleDate={ saleDate }
+          totalPrice={ totalPrice }
+        />
+      ))}
     </div>
   );
 }
