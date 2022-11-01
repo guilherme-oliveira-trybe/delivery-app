4166d3f976fa8fd003d7217372efffdd6f342c14
WalterTNFC <walter.tad@hotmail.com>
2022-11-01 16:21:17 -0300

    feat(8): InvalidRegister Page

---
+++ front-end/src/components/InvalidRegister.jsx
@@ -0,0 +1,13 @@
+function InvalidRegister() {
+  return (
+    <div>
+      <span
+        data-testid="common_register__element-invalid_register"
+      >
+        Registro Inválido!
+      </span>
+    </div>
+  );
+}
+
+export default InvalidRegister;
