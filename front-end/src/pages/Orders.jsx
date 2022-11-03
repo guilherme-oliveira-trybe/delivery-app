28c4c075ac8953c7882a8cc64feafa92751d20f7
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-11-03 18:29:06 -0300

    refactor: adiciona função para buscar o userId no local storage

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import OrderCard from '../components/customerOrders/OrderCard';
 
 export default function Orders() {
   const [customerOrder, setCustomerOrder] = useState([]);
   const [loading, setLoading] = useState(true);
-  // const [userId, setUserId] = useState();
+  const history = useHistory();
+  const [idUser, setIdUser] = useState();
 
   useEffect(() => {
+    const getUserId = () => {
+      if (!localStorage.getItem('user')) {
+        return history.push('/login');
+      }
+      const { id } = JSON.parse(localStorage.getItem('user'));
+      setIdUser(id);
+    };
     const fetchCustomerOrders = async (value) => {
       const url = 'http://www.localhost:3001/customer/orders';
       const response = await fetch(url);
@@ -15,14 +24,15 @@ export default function Orders() {
       const orderByUserId = data.filter((order) => order.userId === value);
       setCustomerOrder(orderByUserId);
     };
-    fetchCustomerOrders(1); // colocar ID do usuário de forma dinamica
+    getUserId();
+    fetchCustomerOrders(idUser); // colocar ID do usuário de forma dinamica
     setLoading(false);
-  }, []);
+  }, [idUser, history]);
 
   return (
     <div>
-      <NavBar />
       { loading && <span>Loading</span>}
+      { !loading && <NavBar />}
       { !loading
       && customerOrder.map(({ id, userId, status, saleDate, totalPrice }, index) => (
         <OrderCard
