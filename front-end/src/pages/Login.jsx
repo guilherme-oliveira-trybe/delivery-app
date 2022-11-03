8c5976613b4f62eae4529acdcfb12d497ee1675a
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-03 18:36:16 -0300

    refactor: correçõa de lint na função do onClick

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -73,7 +73,7 @@ export default function Login() {
               console.log(error);
               setFailedLogin(true);
             }
-          }}
+          } }
         >
           Log In
         </button>
