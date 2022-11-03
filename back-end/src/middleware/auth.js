49623776cbc431ee5f3dde31047b1b5a1c5f90f4
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-03 16:32:39 -0300

    refactor: alteração do segredo do jwt

--- back-end/src/middleware/auth.js
+++ back-end/src/middleware/auth.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET } = process.env;
+const JWT_SECRET = process.env.JWT_SECRET || 'meu_segredo';
 
 const { userService } = require('../services');
 
