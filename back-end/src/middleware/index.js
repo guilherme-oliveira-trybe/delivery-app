af97f66163bcc178bf7c0c1a098537e69b8561b7
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-03 16:30:17 -0300

    feat: criação da referência no index de middleware

--- back-end/src/middleware/index.js
+++ back-end/src/middleware/index.js
@@ -1,3 +1,4 @@
 module.exports.error = require('./error');
 module.exports.auth = require('./auth');
 module.exports.validateSale = require('./validateSale');
+module.exports.createToken = require('./createToken');
