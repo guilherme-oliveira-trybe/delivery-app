9ce76af877d40afe8c035aa35b8c53ee8e20de23
WalterTNFC <walter.tad@hotmail.com>
2022-11-07 18:01:21 -0300

    feat: Create and Delete controller

--- back-end/src/controllers/user.js
+++ back-end/src/controllers/user.js
@@ -35,19 +35,30 @@ const userController = {
   },
 
   create: async (req, res) => {
-    const { name, email, password } = req.body;
-    const user = await userService.create(name, email, password);
+    const user = await userService.create(req.body);
+    if (!user) return res.status(409).json({ error: 'Conflict' });
+    return res.status(201).json({ message: 'Created' });
+  },
+
+  createUserByAdm: async(req, res) => {
+    const { role: roleVerify } = req.user;
+    if (roleVerify !== 'administrator') {
+      return res.status(401).json({ message: 'User is not Adm' })
+    }
+    const user = await userService.create(req.body);
     if (!user) return res.status(409).json({ error: 'Conflict' });
     return res.status(201).json({ message: 'Created' });
   },
 
   delete: async (req, res) => {
-    console.log('Testa deleteeee');
-    const { email } = req.body;
-    console.log(email);
-    await userService.delete(email);
+    const { id } = req.params;
+    console.log(id);
+    const user = await userService.delete(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    };
     return res.status(204).end();
-  }
+  },
 };
 
 module.exports = userController;
