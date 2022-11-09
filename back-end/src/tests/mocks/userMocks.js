dba39b7ed1082f3909bf42648f4a0473f2b7a066
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-11-09 09:45:35 -0300

    test: cria mocks para utilização nos teste das rotas referente a tabela users

---
+++ back-end/src/tests/mocks/userMocks.js
@@ -0,0 +1,14 @@
+const loginMock = {
+  email: 'adm@deliveryapp.com',
+  password: '--adm2@21!!--'
+}
+
+const userMock = {
+  id: 1,
+  name: 'Delivery App Admin',
+  email: 'adm@deliveryapp.com',
+  password: 'a4c86edecc5aee06eff8fdeda69e0d04',
+  role: 'administrator'
+}
+
+module.exports = { loginMock, userMock }
