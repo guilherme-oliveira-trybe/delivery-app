0b17af2b15042407946fcd4e106345e88f5f6ae8
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-11-09 09:46:29 -0300

    test: rota login/ request POST

--- back-end/src/tests/integration/user.test.js
+++ back-end/src/tests/integration/user.test.js
@@ -1,13 +1,15 @@
-import * as sinon from 'sinon';
-import * as chai from 'chai';
+const sinon = require('sinon');
+const chai = require('chai');
 // import * as jwt from 'jsonwebtoken';
-import chaiHttp from 'chai-http';
+const chaiHttp =require('chai-http');
+const { User } = require('../../database/models');
+const { loginMock, userMock } = require('../mocks/userMocks');
 
-// import { app } from '../app';
+const app  = require('../../api/app');
 
 chai.use(chaiHttp);
 
-// const { expect } = chai;
+const { expect } = chai;
 
 // const VALID_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlhdCI6MTY2NDUwNDc4MCwiZXhwIjoxNjY0NTkxMTgwfQ.lYN2ImWYl-ejFGAMEClZzcFS6I3Bx4PX2lfS47v9rus';
 
@@ -16,18 +18,15 @@ chai.use(chaiHttp);
 //   return Promise.resolve({ success: 'Token is valid' });
 // });
 
-describe('Rota /user e /login', () => {
-  describe('Rota POST /', () => {
-    before(async () => sinon.stub(UserModel.prototype, 'findOne').resolves(ADMIN_USER_OBJECT));
-    after(() => (UserModel.prototype.findOne).restore());
+describe('Rota /login', () => {
+  describe('Request POST', () => {
+    before(() => sinon.stub(User, 'findOne').resolves({ dataValues: userMock }));
+    after(() => (User.findOne).restore());
 
-    it('Caso de sucesso', async () => {
-      // const result = await chai.request(app).post('/login').send({
-      //   email: 'admin@admin.com',
-      //   password: 'secret_admin',
-      // });
-      // expect(result.status).to.be.equal(200);
-      // expect(result.body).to.have.property('token');
+    it('Successfully', async () => {
+      const result = await chai.request(app).post('/login').send(loginMock);
+      expect(result.status).to.be.equal(200);
+      expect(result.body).to.have.property('token');
     });
   });
 });
