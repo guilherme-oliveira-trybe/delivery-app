e0bb64c03bdd7e7d3b36e2304d870daf79301627
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-11-10 17:24:41 -0300

    test: adiciona testes para casos de falha

--- back-end/src/routes/sales.js
+++ back-end/src/routes/sales.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const { saleController } = require('../controllers');
-const Middleware = require('../middleware'); 
 const { validateToken } = require('../middleware/auth');
 
 const route = express.Router();
 
 route.get('/customer/orders', validateToken, saleController.getAll);
-// route.get('/customer/orders/:id', saleController.getById);
 route.get('/customer/orders/:id', validateToken, saleController.getById);
 
 route.patch('/customer/orders/:id', saleController.updateStatus);
-route.post(
-  '/customer/orders',
-  validateToken,
-  Middleware.validateSale.validateUsers,
-  Middleware.validateSale.validateAddress,
-  Middleware.validateSale.validateOrder,
-  saleController.create,
-);
+route.post('/customer/orders', validateToken, saleController.create);
 
 route.get('/seller/orders', validateToken, saleController.getAll);
 route.get('/seller/orders/:id', validateToken, saleController.getById);
--- back-end/src/tests/integration/sales.test.js
+++ back-end/src/tests/integration/sales.test.js
@@ -2,8 +2,10 @@ const sinon = require('sinon');
 const chai = require('chai');
 const jwt = require('jsonwebtoken');
 const chaiHttp =require('chai-http');
-const { Sales, User, Product, SalesProduct } = require('../../database/models');
-const { allSalesMock, salesBodyMock, salesCreatedMock, salesById, customer, seller } = require('../mocks/salesMocks');
+const { Sales, SalesProduct } = require('../../database/models');
+const { 
+  allSalesMock, salesBodyMock, salesCreatedMock, salesById, salesUpdate, saleStatusUpdate
+} = require('../mocks/salesMocks');
 
 const app  = require('../../api/app');
 
@@ -13,18 +15,20 @@ const { expect } = chai;
 
 const VALID_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsImlhdCI6MTY2NDUwNDc4MCwiZXhwIjoxNjY0NTkxMTgwfQ.lYN2ImWYl-ejFGAMEClZzcFS6I3Bx4PX2lfS47v9rus';
 
+const INVALID_TOKEN = 'tokenerradodemais'
+
 describe('SalesRoute', () => {
   describe('Rota /customer/orders', () => {
     describe('Request GET', () => {
       before(() => {
-        sinon.stub(jwt, 'verify').resolves();
+        sinon.stub(jwt, 'verify').onCall(0).resolves();
         sinon.stub(Sales, 'findAll').resolves(allSalesMock);
       });
   
       after(() => {
-        (Sales.findAll).restore();
         (jwt.verify).restore();
-      })
+        (Sales.findAll).restore();
+      });
   
       it('Successfully found', async () => {
         const result = await chai.request(app)
@@ -33,24 +37,27 @@ describe('SalesRoute', () => {
         expect(result.status).to.be.equal(200);
         expect(result.body).to.deep.equal(allSalesMock);
       });
+
+      it('Failure Token Not Found', async () => {
+        const result = await chai.request(app)
+        .get('/customer/orders')
+        expect(result.status).to.be.equal(401);
+        expect(result.body).to.deep.equal({ message: 'Token not found' });
+      });
     });
 
     describe('Request Post', () => {
       before(() => {
         sinon.stub(jwt, 'verify').resolves();
-        // sinon.stub(User, 'findAll').onCall(0).resolves(customer).onCall(1).resolves(seller);
-        // sinon.stub(Product, 'findAll').resolves();
-        sinon.stub(Sales, 'create').resolves({ dataValues: salesCreatedMock });
-        sinon.stub(SalesProduct, 'bulkCreate').resolves();
+        sinon.stub(Sales, 'create').onCall(0).resolves({ dataValues: salesCreatedMock }).onCall(1).resolves(null);
+        sinon.stub(SalesProduct, 'bulkCreate').onCall(0).resolves().onCall(1).resolves(null);
       });
   
       after(() => {
         (jwt.verify).restore();
-        // (User.findAll).restore();
-        // (Product.findAll).restore();
         (Sales.create).restore();
         (SalesProduct.bulkCreate).restore();
-      })
+      });
 
       it('Successfully created', async () => {
         const result = await chai.request(app)
@@ -60,7 +67,16 @@ describe('SalesRoute', () => {
         expect(result.status).to.be.equal(201);
         expect(result.body).to.deep.equal(salesCreatedMock);
       });
-    })
+
+      it('Failure', async () => {
+        const result = await chai.request(app)
+        .post('/customer/orders')
+        .send({})
+        .set({ 'authorization': `${VALID_TOKEN}` });
+        expect(result.status).to.be.equal(404);
+        expect(result.body).to.deep.equal({ message: 'Can\'t create sale' });
+      });
+    });
   });
 
   describe('Rota /customer/orders/:id', () => {
@@ -73,7 +89,7 @@ describe('SalesRoute', () => {
       after(() => {
         (Sales.findByPk).restore();
         (jwt.verify).restore();
-      })
+      });
 
       it('Successfully found', async () => {
         const result = await chai.request(app)
@@ -83,5 +99,77 @@ describe('SalesRoute', () => {
         expect(result.body).to.deep.equal([salesById]);
       });
     })
+
+    describe('Request PATCH', () => {
+      before(() => {
+        sinon.stub(Sales, 'findAll').onCall(0).resolves(salesById).onCall(1).resolves(salesUpdate).onCall(2).resolves(null);
+        sinon.stub(Sales, 'update').resolves();
+      });
+  
+      after(() => {
+        (Sales.findAll).restore();
+        (Sales.update).restore();
+      });
+
+      it('Successfully changed', async () => {
+        const result = await chai.request(app)
+        .patch(`/customer/orders/${salesCreatedMock.id}`)
+        .send(saleStatusUpdate)
+        expect(result.status).to.be.equal(200);
+        expect(result.body).to.deep.equal(salesUpdate);
+      });
+
+      it('Failure', async () => {
+        const result = await chai.request(app)
+        .patch(`/customer/orders/${salesCreatedMock.id, 'Entregue'}`)
+        .set({ 'authorization': `${VALID_TOKEN}` });
+        expect(result.status).to.be.equal(404);
+        expect(result.body).to.deep.equal({ message: 'Can\'t find sale' });
+      });
+    })
   })
+
+  describe('Rota /seller/orders', () => {
+    describe('Request GET', () => {
+      before(() => {
+        sinon.stub(jwt, 'verify').resolves();
+        sinon.stub(Sales, 'findAll').resolves(allSalesMock);
+      });
+  
+      after(() => {
+        (Sales.findAll).restore();
+        (jwt.verify).restore();
+      });
+  
+      it('Successfully found', async () => {
+        const result = await chai.request(app)
+        .get('/seller/orders')
+        .set({ 'authorization': `${VALID_TOKEN}` });
+        expect(result.status).to.be.equal(200);
+        expect(result.body).to.deep.equal(allSalesMock);
+      });
+    });
+  });
+
+  describe('Rota /seller/orders/:id', () => {
+    describe('Request GET', () => {
+      before(() => {
+        sinon.stub(jwt, 'verify').resolves();
+        sinon.stub(Sales, 'findByPk').resolves(salesById);
+      });
+  
+      after(() => {
+        (Sales.findByPk).restore();
+        (jwt.verify).restore();
+      });
+
+      it('Successfully found', async () => {
+        const result = await chai.request(app)
+        .get(`/seller/orders/${salesCreatedMock.id}`)
+        .set({ 'authorization': `${VALID_TOKEN}` });
+        expect(result.status).to.be.equal(200);
+        expect(result.body).to.deep.equal([salesById]);
+      });
+    });
+  });
 });
