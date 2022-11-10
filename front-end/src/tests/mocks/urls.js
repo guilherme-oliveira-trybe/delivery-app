41d5f253b04a58a8b2f3691df7361f68ab64e84d
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-10 14:44:16 -0300

    feat: estruturação inicial das requisições mockadas

--- front-end/src/tests/mocks/urls.js
+++ front-end/src/tests/mocks/urls.js
@@ -1,7 +1,32 @@
-// import meals from './meals';
+import allProducts from './products.mock';
+import sales from './sales.mock';
+import {
+  allUsers,
+  allUsersSellers,
+  userOne,
+  userThree,
+  userTwo,
+} from './users.mock';
 
 const URLS = {
-  key: 1,
+  'http://localhost:3001/user': allUsers,
+  'http://localhost:3001/user/role/seller': allUsersSellers,
+  'http://localhost:3001/user/1': userOne,
+  'http://localhost:3001/user/2': userTwo,
+  'http://localhost:3001/user/3': userThree,
+  'http://localhost:3001/products': allProducts,
+  'http://localhost:3001/products/1': { product: [allProducts[0]] },
+  'http://localhost:3001/products/2': { product: [allProducts[1]] },
+  'http://localhost:3001/products/3': { product: [allProducts[2]] },
+  'http://localhost:3001/products/4': { product: [allProducts[3]] },
+  'http://localhost:3001/products/5': { product: [allProducts[4]] },
+  'http://localhost:3001/products/6': { product: [allProducts[5]] },
+  'http://localhost:3001/products/7': { product: [allProducts[6]] },
+  'http://localhost:3001/products/8': { product: [allProducts[7]] },
+  'http://localhost:3001/products/9': { product: [allProducts[8]] },
+  'http://localhost:3001/products/10': { product: [allProducts[9]] },
+  'http://localhost:3001/products/11': { product: [allProducts[10]] },
+  'url sales aqui': sales,
 };
 
 export default URLS;
