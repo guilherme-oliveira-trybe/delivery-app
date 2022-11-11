25637367ec5038cc7c56714f4ea1392a8954aaec
Iasmin-BR <iasmingabrig@gmail.com>
2022-11-11 12:37:22 -0300

    style(navbar): add basic style

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
+import './styles/NavBar.css';
 
 function NavBar() {
   const user = JSON.parse(localStorage.getItem('user'));
 
   return (
     <nav className="nav-bar">
-      <div className="box">
-        <a
-          href="/customer/products"
-          data-testid="customer_products__element-navbar-link-products"
-        >
-          PRODUTOS
-        </a>
-        <a
-          href="/customer/orders"
-          data-testid="customer_products__element-navbar-link-orders"
-        >
-          MEUS PEDIDOS
-        </a>
-      </div>
-      <div className="box">
-        <p
-          data-testid="customer_products__element-navbar-user-full-name"
-        >
-          { user.name }
-        </p>
-        <a
-          href="/"
-          data-testid="customer_products__element-navbar-link-logout"
-          onClick={ () => localStorage.clear() }
-        >
-          Sair
-        </a>
-      </div>
+      <ul>
+        <li>
+          <a
+            href="/customer/products"
+            data-testid="customer_products__element-navbar-link-products"
+          >
+            PRODUTOS
+          </a>
+        </li>
+        <li>
+          <a
+            href="/customer/orders"
+            data-testid="customer_products__element-navbar-link-orders"
+          >
+            MEUS PEDIDOS
+          </a>
+        </li>
+        <li>
+          <span
+            data-testid="customer_products__element-navbar-user-full-name"
+          >
+            { user.name }
+          </span>
+        </li>
+        <li>
+          <a
+            href="/"
+            data-testid="customer_products__element-navbar-link-logout"
+            onClick={ () => localStorage.clear() }
+          >
+            SAIR
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
