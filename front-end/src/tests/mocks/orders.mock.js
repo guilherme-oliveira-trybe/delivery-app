3967ce3ee2208f280893866ffaed3946c30a57dd
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-11-14 00:38:30 -0300

    refactor: atualiza as vendas mockadas

--- front-end/src/tests/mocks/orders.mock.js
+++ front-end/src/tests/mocks/orders.mock.js
@@ -1,7 +1,34 @@
-export const firstOrder = {};
+export const firstOrder = {
+  id: 1,
+  userId: 3,
+  sellerId: 2,
+  totalPrice: '9.70',
+  deliveryAddress: 'Rio um',
+  deliveryNumber: '1',
+  saleDate: '2022-11-14T02:23:35.000Z',
+  status: 'Pendente',
+};
 
-export const secondOrder = {};
+export const secondOrder = {
+  id: 2,
+  userId: 3,
+  sellerId: 2,
+  totalPrice: '17.78',
+  deliveryAddress: 'Rua Dois',
+  deliveryNumber: '2',
+  saleDate: '2022-11-14T02:24:01.000Z',
+  status: 'Pendente',
+};
 
-export const thirdOrder = {};
+export const thirdOrder = {
+  id: 3,
+  userId: 3,
+  sellerId: 2,
+  totalPrice: '17.85',
+  deliveryAddress: 'Rua Três',
+  deliveryNumber: '3',
+  saleDate: '2022-11-14T02:24:26.000Z',
+  status: 'Pendente',
+};
 
 export const ordersMock = [firstOrder, secondOrder, thirdOrder];
